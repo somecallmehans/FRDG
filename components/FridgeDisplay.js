b3786396c6a9f787a033e36dbee23d13aa3d519e
Christian Anderson <somecallmehans@gmail.com>
2022-01-12 16:48:00 -0500

    Added loading spinner + updated empty fridge screen

--- components/FridgeDisplay.js
+++ components/FridgeDisplay.js
@@ -7,6 +7,7 @@ import {
   TouchableOpacity,
   FlatList,
   Dimensions,
+  ActivityIndicator,
 } from "react-native";
 import { ListItem, Button } from "react-native-elements";
 import { useNavigation } from "@react-navigation/native";
@@ -19,10 +20,12 @@ const windowHeight = Dimensions.get("window").height;
 
 const fridgeDisplay = (props) => {
   const [fridge, setFridge] = useState([]);
+  const [loading, setLoading] = useState(true);
   const [day, setDay] = useState("");
 
   useEffect(async () => {
     await props.getUserFridge();
+    setLoading(false);
     const today = new Date();
     setDay(today);
   }, []);
@@ -36,52 +39,74 @@ const fridgeDisplay = (props) => {
     return Math.round(timeDiff / oneDay);
   };
 
-  return (
-    <View style={styles.container}>
-      {props.fridge.length > 0 ? (
-        <FlatList
-          data={props.fridge}
-          extraData={props.fridge}
-          renderItem={({ item }) => {
-            const expTime = setNumberOfDays(item.dateExpires, day);
-            return (
-              <ListItem.Swipeable
-                bottomDivider
-                rightContent={
-                  <Button
-                    title="Delete"
-                    titleStyle={styles.buttonTitle}
-                    onPress={() => props.removeFromFridge(item.foodId)}
-                    buttonStyle={styles.button}
-                  />
-                }
-                key={item.foodId}
-              >
-                <ListItem.Content>
-                  <ListItem.Title>{item.addedFoodName}</ListItem.Title>
-                </ListItem.Content>
-                <ListItem.Content right>
-                  <ListItem.Title right>
-                    {expTime > 0 ? expTime : "Expired!"}
-                  </ListItem.Title>
-                </ListItem.Content>
-              </ListItem.Swipeable>
-            );
-          }}
-          keyExtractor={(item, index) => index.toString()}
-        />
-      ) : (
-        <View>
-          <Text>Add items to fridge to begin!</Text>
-        </View>
-      )}
-    </View>
-  );
+  if (loading) {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        {props.fridge.length > 0 ? (
+          <FlatList
+            data={props.fridge}
+            extraData={props.fridge}
+            renderItem={({ item }) => {
+              const expTime = setNumberOfDays(item.dateExpires, day);
+              return (
+                <ListItem.Swipeable
+                  bottomDivider
+                  rightContent={
+                    <Button
+                      title="Delete"
+                      titleStyle={styles.buttonTitle}
+                      onPress={() => props.removeFromFridge(item.foodId)}
+                      buttonStyle={styles.button}
+                    />
+                  }
+                  key={item.foodId}
+                >
+                  <ListItem.Content>
+                    <ListItem.Title>{item.addedFoodName}</ListItem.Title>
+                  </ListItem.Content>
+                  <ListItem.Content right>
+                    <ListItem.Title right>
+                      {expTime > 0 ? expTime : "Expired!"}
+                    </ListItem.Title>
+                  </ListItem.Content>
+                </ListItem.Swipeable>
+              );
+            }}
+            keyExtractor={(item, index) => index.toString()}
+          />
+        ) : (
+          <View>
+            <Text style={styles.textContainer}>Your fridge is empty!</Text>
+          </View>
+        )}
+      </View>
+    );
+  }
 };
 
 const styles = StyleSheet.create({
   container: {
     height: windowHeight,
+  },
+  loadingContainer: {
+    height: windowHeight,
+    position: "absolute",
+    justifyContent: "center",
+    left: 0,
+    right: 0,
+    top: 0,
+  },
+  textContainer: {
+    textAlign: "center",
+    fontSize: 30,
+    fontWeight: "700",
+    marginTop: "50%",
   },
   button: {
     backgroundColor: "#FF0000",
